596b942e7dd9ffceb2448b96e5dfca6050669871
Marco L. Abreu <marcolabreu@royalmail.top>
2019-04-07 17:49:48 +0200

    Fill meta defaults except OGImage

--- components/head.js
+++ components/head.js
@@ -2,35 +2,32 @@ import React from 'react'
 import NextHead from 'next/head'
 import { string } from 'prop-types'
 
-const defaultDescription = ''
-const defaultOGURL = ''
+const defaultDescription = 'All characters from the Rick and Morty TV show in an easy to browse and filter app'
+const defaultOGURL = 'https://rick-reacts.now.sh'
+// TODO: prepare 1200x1200 image
 const defaultOGImage = ''
 
 const Head = props => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{props.title || ''}</title>
-    <meta
-      name="description"
-      content={props.description || defaultDescription}
-    />
+    <title>{props.title || 'Rick and Morty Characters'}</title>
+
+    <meta name="description" content={props.description || defaultDescription} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    <link rel="icon" sizes="196x196" type="image/png" href="/static/morty-196x196.png" />
-    <link rel="apple-touch-icon" href="/static/morty-152x152.png" />
-    <link rel="mask-icon" href="/static/morty-196x196.svg" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
-    <meta
-      property="og:description"
-      content={props.description || defaultDescription}
-    />
+    <meta property="og:description" content={props.description || defaultDescription} />
     <meta name="twitter:site" content={props.url || defaultOGURL} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link rel="icon" sizes="196x196" type="image/png" href="/static/morty-196x196.png" />
+    <link rel="apple-touch-icon" href="/static/morty-152x152.png" />
+    <link rel="mask-icon" href="/static/morty-196x196.svg" />
   </NextHead>
 )
 
