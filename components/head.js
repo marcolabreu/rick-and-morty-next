fbff5d15738305746924439c4996fdca0e3e7f28
Marco L. Abreu <marcolabreu@royalmail.top>
2019-04-07 16:46:15 +0200

    Add icons: our favicons and Material Design link

--- components/head.js
+++ components/head.js
@@ -15,10 +15,10 @@ const Head = props => (
       content={props.description || defaultDescription}
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link rel="icon" sizes="196x196" type="image/png" href="/static/morty-196x196.png" />
+    <link rel="apple-touch-icon" href="/static/morty-152x152.png" />
+    <link rel="mask-icon" href="/static/morty-196x196.svg" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
     <meta
